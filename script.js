78878bae0238768888010ec96ec17b7c0a5b9c96
Bikesh-hub <thounaojambikeshana123@gmail.com>
2026-04-13 18:56:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function contactWhatsApp() {
+  window.open("https://wa.me/918837016756", "_blank");
+}
+
+function enquire(product) {
+  let message = `Hello, I am interested in ${product}`;
+  let url = `https://wa.me/918837016756?text=${encodeURIComponent(message)}`;
+  window.open(url, "_blank");
+}
